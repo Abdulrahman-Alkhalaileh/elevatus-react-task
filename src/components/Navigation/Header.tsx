e4a1893fd34189bb3f4e55eb58982dbde3f0a486
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-08-20 02:34:19 +0300

    added a basic header with the logo icon

---
+++ src/components/Navigation/Header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box, BoxProps } from "@mui/material";
+import Logo from "./Logo";
+
+export interface HeaderProps extends BoxProps {}
+
+const Header: React.FC<HeaderProps> = ({ ...props }) => {
+  return (
+    <Box
+      py={3}
+      px={8}
+      bgcolor="primary.main"
+      display="flex"
+      position="sticky"
+      top={0}
+      zIndex={1000}
+      {...props}
+    >
+      <Logo />
+    </Box>
+  );
+};
+
+export default Header;
---
+++ src/components/Navigation/Logo.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+
+export interface LogoProps {
+  width?: number;
+}
+
+const Logo: React.FC<LogoProps> = ({ width }) => {
+  return (
+    <>
+      <img
+        width={width || 150}
+        alt="Logo"
+        src="https://elevatus-production.s3.eu-central-1.amazonaws.com/career_portal/2021/05/15084eba-33e0-4a8a-b74c-8a158d12b6b2/original/y2ZqILiIdB5rmdH5XuG6ujLRHRJbweiyMbSrsYjN.png"
+      />
+    </>
+  );
+};
+
+export default Logo;
