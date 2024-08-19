c13de0ee47550b291f8d595ff11e2c7391d6e281
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-08-20 02:26:25 +0300

    added the router and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Navigation/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <>
+      <Header />
+      <Box py={3} px={8}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/about" element={<h1>About</h1>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="*" element={<h1>404 Not Found</h1>}/>
+          </Routes>
+        </BrowserRouter>
+      </Box>
+    </>
   );
 }
 
