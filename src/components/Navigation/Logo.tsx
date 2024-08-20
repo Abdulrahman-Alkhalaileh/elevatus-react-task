c4bf15880b6d3ef91fbfbf8d0444045dd032d7f5
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-08-20 22:37:24 +0300

    add a link to logo to navigate into home page

--- src/components/Navigation/Logo.tsx
+++ src/components/Navigation/Logo.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export interface LogoProps {
   width?: number;
@@ -6,13 +7,13 @@ export interface LogoProps {
 
 const Logo: React.FC<LogoProps> = ({ width }) => {
   return (
-    <>
+    <Link to={'/'}>
       <img
         width={width || 150}
         alt="Logo"
         src="https://elevatus-production.s3.eu-central-1.amazonaws.com/career_portal/2021/05/15084eba-33e0-4a8a-b74c-8a158d12b6b2/original/y2ZqILiIdB5rmdH5XuG6ujLRHRJbweiyMbSrsYjN.png"
       />
-    </>
+    </Link>
   );
 };
 
