e1141c91d0ddbed5798ff1a369d6734669a29837
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-08-21 01:03:46 +0300

    connect translation with local storage to prevent reset on reload page

--- src/components/Navigation/LanguageButton.tsx
+++ src/components/Navigation/LanguageButton.tsx
@@ -8,19 +8,27 @@ const LanguageButton: React.FC<LanguageButtonProps> = ({ ...props }) => {
   const { i18n, t } = useTranslation();
 
   useEffect(() => {
+    i18n.changeLanguage(localStorage.getItem("language") || "en");
     // Change direction based on the selected language
     document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
-  }, [i18n.language]);
+  }, [i18n.language, i18n]);
 
   const toggleLanguage = () => {
     const languages = ["ar", "en"];
     const selectedLanguage = i18n.language;
-    i18n.changeLanguage(languages.find((lan) => lan !== selectedLanguage)); // Change the language
+    const nextLanguage = languages.find((lan) => lan !== selectedLanguage);
+    i18n.changeLanguage(nextLanguage); // Change the language
+    localStorage.setItem("language", nextLanguage || "en");
   };
 
   return (
     <>
-      <Button onClick={() => toggleLanguage()} color="secondary" variant="contained" sx={{fontWeight:800,fontSize:15,py:0,borderRadius:5}}>
+      <Button
+        onClick={() => toggleLanguage()}
+        color="secondary"
+        variant="contained"
+        sx={{ fontWeight: 800, fontSize: 15, py: 0, borderRadius: 5 }}
+      >
         {t("language")}
       </Button>
     </>
