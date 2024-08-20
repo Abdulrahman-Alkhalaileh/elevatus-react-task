8e1b42e4a66f699e7a24defaa1db6f53f1e92516
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-08-20 22:34:15 +0300

    added footer and include it and header in the browser router

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 import Home from "./pages/Home";
 import JobInformation from "./pages/JobInformation";
+import Footer from "./components/Navigation/Footer";
 
 function App() {
   return (
     <>
-      <Header />
-      <Box
-        py={3}
-        px={{ xs: 2, md: 8 }}
-        minHeight="83.7vh"
-        bgcolor="background.default"
-      >
-        <BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Box
+          py={3}
+          px={{ xs: 2, md: 8 }}
+          minHeight="83.7vh"
+          bgcolor="background.default"
+        >
           <Routes>
             <Route path="/about" element={<h1>About</h1>} />
             <Route path="/" element={<Home />} />
-            <Route path="/:slug" element={<JobInformation/>} />
+            <Route path="/:slug" element={<JobInformation />} />
             <Route path="*" element={<h1>404 Not Found</h1>} />
           </Routes>
-        </BrowserRouter>
-      </Box>
+        </Box>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
